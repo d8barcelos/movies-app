b624a22ee0bd7bb748779de8195d56d19594daa7
Diogo <d8barcelos@gmail.com>
2023-05-03 18:14:23 -0300

    commit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react"
+import { useSearchParams } from "react-router-dom"
+import movieCard from "../components/movieCard"
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
+import "./MovieGrid.css";
 
 const Search = () => {
+  const [searchParams] = useSearchParams()
+
+  const [movies, setMovies] = useState([])
+  const query = searchParams.get("q")
   return (
-    <div>Search</div>
+    <div className="container">
+        <h2 className="title">Results for: <span className="query-text">{query}</span>
+        </h2>
+        <div className="movies-container">
+        {topMovies.length === 0 && <p>Loading...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <movieCard key={movie.id} movie={movie}/>)}
+        </div>
+      </div>
   )
 }
 
