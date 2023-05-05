a4df2e54a6313aba8dbf15e260de9cd3be196068
Diogo <d8barcelos@gmail.com>
2023-05-05 11:21:35 -0300

    commit

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import {
-  BsGraphUp,
   BsWallet2,
   BsHourglassSplit,
+  BsFillCalendarDateFill,
   BsFillFileEarmarkTextFill
 } from 'react-icons/bs'
 
@@ -24,16 +24,48 @@ const Movie = () => {
     setMovie(data);
   }
 
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "usd",
+    })
+  } 
+
   useEffect(() => {
     const movieURL = `${moviesURL}${id}?${apiKey}`
     getMovie(movieURL)
   }, [id])
 
   return (
-    <div>
-    {movie && <>
-      <MovieCard movie={movie} />
-    </>}
+    <div className="movie-page">
+      {movie && <>
+        <MovieCard movie={movie} showLink={false} />
+        <p className="tagline">{movie.tagline}</p>
+        <div className="info">
+          <h3>
+            <BsWallet2 /> Budget
+            <p>{formatCurrency(movie.budget)}</p>
+          </h3>
+        </div>
+        <div className="info">
+          <h3>
+            <BsFillCalendarDateFill /> Release date
+            <p>{movie.release_date}</p>
+          </h3>
+        </div>
+        <div className="info">
+          <h3>
+            <BsHourglassSplit /> Duration
+            <p>{movie.runtime} minutes</p>
+          </h3>
+        </div>
+        <div className="info description">
+          <h3>
+            <BsFillFileEarmarkTextFill /> Overview
+            <p>{movie.overview}</p>
+          </h3>
+        </div>
+      </>}
     </div>
   )
 }
